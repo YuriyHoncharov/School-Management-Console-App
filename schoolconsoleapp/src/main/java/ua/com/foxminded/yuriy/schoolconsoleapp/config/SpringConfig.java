52c087c06b8f93820a66aef2e18a29c9c4c0e80e
Yuriy Honcharov <yuriyhoncharov@gmail.com>
2023-09-28 01:37:31 +0200

    28.09.2023
    1. Added @Bean flyway.migrate()

--- schoolconsoleapp/src/main/java/ua/com/foxminded/yuriy/schoolconsoleapp/config/SpringConfig.java
+++ schoolconsoleapp/src/main/java/ua/com/foxminded/yuriy/schoolconsoleapp/config/SpringConfig.java
@@ -2,6 +2,7 @@ package ua.com.foxminded.yuriy.schoolconsoleapp.config;
 
 import javax.sql.DataSource;
 
+import org.flywaydb.core.Flyway;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -35,7 +36,20 @@ public class SpringConfig {
 		dataSource.setPassword(password);
 		return dataSource;
 	}
-	
+
+	@Bean
+	public Flyway flyway() {
+	    Flyway flyway = Flyway.configure()
+	        .dataSource(url, username, password)
+	        .locations("classpath:db/migration")
+	        .baselineOnMigrate(true)
+	        .baselineVersion("1.0") // Adjust this to match the version in your migration script
+	        .load();
+
+	    flyway.migrate();
+	    return flyway;
+	}
+
 	@Bean
 	public JdbcTemplate jdbcTemplate() {
 		return new JdbcTemplate(dataSource());
