1ac706323fed46592adb34cb8fb2562afbb0e8f2
Yuriy Honcharov <yuriyhoncharov@gmail.com>
2023-11-09 01:58:55 +0100

    09.11.2023
    Modified tests using mockito in CourseDaoImplTests.

--- schoolconsoleapp/src/test/java/ua/com/foxminded/yuriy/schoolconsoleapp/dao/impl/CourseDaoImplTest.java
+++ schoolconsoleapp/src/test/java/ua/com/foxminded/yuriy/schoolconsoleapp/dao/impl/CourseDaoImplTest.java
@@ -1,6 +1,7 @@
 package ua.com.foxminded.yuriy.schoolconsoleapp.dao.impl;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
@@ -16,55 +17,44 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.tree.RowMapper;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatcher;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockedStatic;
 import org.mockito.MockitoAnnotations;
-import ua.com.foxminded.yuriy.schoolconsoleapp.config.ConnectionUtil;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
+
 import ua.com.foxminded.yuriy.schoolconsoleapp.dao.constants.sqlqueries.SqlCourseQueries;
 import ua.com.foxminded.yuriy.schoolconsoleapp.dao.constants.tables.CoursesColumns;
+import ua.com.foxminded.yuriy.schoolconsoleapp.dao.mappers.CourseMapper;
 import ua.com.foxminded.yuriy.schoolconsoleapp.entity.Course;
 
+@ExtendWith(MockitoExtension.class)
 public class CourseDaoImplTest {
 
 	@Mock
-	private Connection mockConnection;
-	@Mock
-	private PreparedStatement mockStatement;
-	@Mock
-	private ResultSet mockResultSet;
-
-	private MockedStatic<ConnectionUtil> mockedStatic;
+	private JdbcTemplate jdbcTemplate;
+	@InjectMocks
 	private CourseDaoImpl courseDao;
-
-	@BeforeEach
-	void setUp() throws SQLException {
-		MockitoAnnotations.openMocks(this);
-		mockedStatic = mockStatic(ConnectionUtil.class);
-		when(ConnectionUtil.getConnection()).thenReturn(mockConnection);
-		when(mockConnection.prepareStatement(anyString())).thenReturn(mockStatement);
-		courseDao = new CourseDaoImpl();
-	}
-
-	@AfterEach
-	void cleanUp() {
-		mockedStatic.close();
-	}
 
 	@Test
 	void addAllTest_Success() throws SQLException {
 		List<Course> courses = new ArrayList<>();
 		courses.add(new Course("Mathematics", "Math Course", 1));
 		courses.add(new Course("Biology", "Biology Course", 2));
-		when(mockStatement.executeUpdate()).thenReturn(1);
+		when(jdbcTemplate.update(anyString(), any(), any(), any())).thenReturn(1);
 		courseDao.addAll(courses);
-		verify(mockConnection, times(1)).prepareStatement(SqlCourseQueries.ADD_ALL);
-		verify(mockStatement, times(courses.size())).setInt(eq(1), anyInt());
-		verify(mockStatement, times(courses.size())).setString(eq(2), anyString());
-		verify(mockStatement, times(courses.size())).setString(eq(3), anyString());
-		verify(mockStatement, times(courses.size())).executeUpdate();
+		verify(jdbcTemplate, times(courses.size())).update(eq(SqlCourseQueries.ADD_ALL), anyInt(), anyString(),
+				anyString());
+
 	}
 
 	@Test
@@ -73,23 +63,14 @@ public class CourseDaoImplTest {
 		List<Course> courses = new ArrayList<>();
 		courses.add(new Course("Mathematics", "Math Course", 1));
 		courses.add(new Course("Biology", "Biology Course", 2));
-		when(mockStatement.executeQuery()).thenReturn(mockResultSet);
-		when(mockResultSet.next()).thenReturn(true, true, false);
-		when(mockResultSet.getString(CoursesColumns.COURSE_NAME)).thenReturn("Mathematics").thenReturn("Biology");
-		when(mockResultSet.getString(CoursesColumns.COURSE_DESCRIPTION)).thenReturn("Math Course")
-				.thenReturn("Biology Course");
-		when(mockResultSet.getInt(CoursesColumns.COURSE_ID)).thenReturn(1).thenReturn(2);
+		when(jdbcTemplate.query(eq(SqlCourseQueries.GET_COURSES_BY_STUDENT_ID), any(CourseMapper.class), eq(studentId)))
+				.thenReturn(courses);
 		List<Course> result = courseDao.getByStudentId(studentId);
-		assertEquals(courses.size(), result.size());
-		verify(mockConnection, times(1)).prepareStatement(SqlCourseQueries.GET_COURSES_BY_STUDENT_ID);
-		verify(mockStatement, times(1)).setInt(eq(1), anyInt());
-		verify(mockStatement, times(1)).executeQuery();
-		verify(mockResultSet, times(3)).next();
-		verify(mockResultSet, times(2)).getString(CoursesColumns.COURSE_NAME);
-		verify(mockResultSet, times(2)).getString(CoursesColumns.COURSE_DESCRIPTION);
-		verify(mockResultSet, times(2)).getInt(CoursesColumns.COURSE_ID);
+		verify(jdbcTemplate, times(1)).query(eq(SqlCourseQueries.GET_COURSES_BY_STUDENT_ID), any(CourseMapper.class),
+				eq(studentId));
+		assertEquals(courses, result);
 	}
-	
+
 	@Test
 	void addToStudentTest_Success_shouldAddCourseToStudent() throws SQLException {
 		int studentId = 10;
@@ -100,6 +81,6 @@ public class CourseDaoImplTest {
 		verify(mockStatement, times(1)).setInt(1, course.getId());
 		verify(mockStatement, times(1)).setInt(2, studentId);
 		verify(mockStatement, times(1)).executeUpdate();
-		
+
 	}
 }
